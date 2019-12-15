a582874095f6f6a46a52f3fa82f2311b15c2f002
Leon Jamison <leonjamison@icloud.com>
2019-12-15 05:28:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,20 +1,41 @@
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-music_library.each do |artist_array|
-  artist_array.each do |artist_element|
-    if artist_element.class != Array
-      puts "Artist: #{artist_element}"
-    else
-      artist_element.each do |album_element|
-        if album_element.class != Array
-          puts "Album: #{album_element}"
-        else
-          album_element.each do |song_element|
-            puts "Song: #{song_element}"
-          end
-        end
-      end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
     end
   end
+
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
